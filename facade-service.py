1a89cee817d45445c26f7c13916695a40a1cdfc7
ango-ipt <ango-ipt22@lll.kpi.ua>
2021-06-15 06:37:32 +0300

    micro_mq commit

--- facade-service.py
+++ facade-service.py
@@ -1,21 +1,46 @@
+import random
 import uuid
 import requests
+import pika
 from models import FacadePostMessage
 from fastapi import FastAPI
 
-MESSAGE_SERVICE = 'http://127.0.0.1:8001/service/'
-LOGGING_SERVICE = 'http://127.0.0.1:8002/service/'
+MESSAGE_HOSTS = ('http://127.0.0.1:8001/service/',
+                 'http://127.0.0.1:8002/service/')
+
+LOGGING_HOSTS = ('http://127.0.0.1:9001/service/',
+                 'http://127.0.0.1:9002/service/',
+                 'http://127.0.0.1:9003/service/')
+
+
+def get_log_host():
+    return random.choice(LOGGING_HOSTS)
+
+
+def get_msg_host():
+    return random.choice(MESSAGE_HOSTS)
+
+
+def put_msg_mq(msg):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='lab6')
+    channel.basic_publish(exchange='', routing_key='lab6', body=msg)
+    connection.close()
+
 
 app = FastAPI()
 
 
 @app.get('/facade_service/')
 def get():
-    message_response_text = requests.get(MESSAGE_SERVICE).text.strip('"')
-    logging_response_text = requests.get(LOGGING_SERVICE).text.strip('"')
+    logging_response_text = requests.get(get_log_host()).text.strip('"')
+    message_response_text = requests.get(get_msg_host()).text.strip('"')
     return logging_response_text + ': ' + message_response_text
 
 
 @app.post("/facade_service/", status_code=200)
 def post(msg: FacadePostMessage):
-    requests.post(url=LOGGING_SERVICE, json={'uuid': str(uuid.uuid4()), 'msg': msg.msg})
+    requests.post(url=get_log_host(), json={'uuid': str(uuid.uuid4()), 'msg': msg.msg})
+    put_msg_mq(msg.msg)
--- logging-service.py
+++ logging-service.py
@@ -1,17 +1,18 @@
+import hazelcast
 from models import Message
 from fastapi import FastAPI
 
 app = FastAPI()
 
-uuid_messages = dict()
+uuid_message_map = hazelcast.HazelcastClient().get_map('my_map')
 
 
 @app.post('/service/', status_code=200)
 def post(message: Message):
-    uuid_messages[message.uuid] = message.msg
+    uuid_message_map.put(message.uuid, message.msg)
     print(message)
 
 
 @app.get('/service/')
 def get():
-    return '[{}]'.format(', '.join(uuid_messages.values()))
+    return str([msg for (uid, msg) in uuid_message_map.entry_set().result()])
